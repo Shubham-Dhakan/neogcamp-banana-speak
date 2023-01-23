7edb85e9fe83d2d67e7bb5232bbfb6157ec18b1d
Shubham Bhavin Dhakan <103954473+Shubham-Dhakan@users.noreply.github.com>
2023-01-22 19:29:41 -0500

    Commit modified and new files to remote rep[o

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("txt-input");
+
+function clickHandler() {
+    console.log("clicker");
+    console.log("input"+txtInput.value);
+
+}
+
+addEventListener.addEventListener("click",clickHandler)
