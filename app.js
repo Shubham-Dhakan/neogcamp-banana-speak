c60ed3356dc361c7bcfbad43216126a4a97d44d6
Shubham Bhavin Dhakan <103954473+Shubham-Dhakan@users.noreply.github.com>
2023-01-24 08:33:24 -0500

    committing updated files to repo

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var btnTranslate = document.querySelector("#btn-translate");
 var inputDiv = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-//var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url = "https://api.funtranslations.com/translate/minion.json"
 
-var url = "https://api.funtranslations.com/translate/australian.json"
+//var url = "https://api.funtranslations.com/translate/australian.json"
 
 function getTranslationURL(Text) {
     return url + "?" + "text=" + Text
@@ -19,9 +19,12 @@ function clickHandler() {
     var inputText = inputDiv.value;
     fetch(getTranslationURL(inputText))
         .then(respone => respone.json())
-        .then(json => console.log(json.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
         .catch(errorHandler)
-    };
+                        };
 
 btnTranslate.addEventListener("click",clickHandler);
 
